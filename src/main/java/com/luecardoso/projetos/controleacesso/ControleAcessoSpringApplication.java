06b2b389295e417254cabe4d6c4ed5dc00b4fed2
lucas reis <lucasreis-@outlook.com>
2025-04-24 11:25:18 -0300

    iniciando projeto

---
+++ src/main/java/com/luecardoso/projetos/controleacesso/ControleAcessoSpringApplication.java
@@ -0,0 +1,13 @@
+package com.luecardoso.projetos.controleacesso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleAcessoSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControleAcessoSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/luecardoso/projetos/controleacesso/ControleAcessoSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luecardoso.projetos.controleacesso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleAcessoSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
